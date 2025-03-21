4296d415cb49d51985c88424a555121fd8d4b57b
Motorin Ilya <motorin_0404@mail.ru>
2025-03-22 00:22:44 +0700

    first commit

---
+++ Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace ICGFilter.Presentation.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Presentation/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ICGFilter.Presentation.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
